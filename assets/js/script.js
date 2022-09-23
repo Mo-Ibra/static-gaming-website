b8091e5ea5f8745169f0e7b0a9e567094a30c914
Dev-Maradona <maradona.0000@yahoo.com>
2022-09-23 07:45:13 -0700

    First Commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navbarToggler = document.querySelector("[data-nav-toggler]");
+
+navbarToggler.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+});
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.remove("active");
+    navbarToggler.classList.remove("active");
+  });
+}
