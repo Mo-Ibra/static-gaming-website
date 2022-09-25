970bdbdbb9bbb464d99d7db513a4d0d161226f4c
Dev-Maradona <maradona.0000@yahoo.com>
2022-09-25 10:52:05 -0700

    Added Add to top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,24 @@ const searchToggler = document.querySelectorAll("[data-search-toggler]");
 const searchBox = document.querySelector("[data-search-box]");
 
 for (let i = 0; i < searchToggler.length; i++) {
-    searchToggler[i].addEventListener('click', function() {
-        searchBox.classList.toggle('active');
-    });
+  searchToggler[i].addEventListener('click', function () {
+    searchBox.classList.toggle('active');
+  });
 }
+
+/**
+ * header
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 200) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
